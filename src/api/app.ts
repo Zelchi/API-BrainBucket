17ce39d91859eeab5fceb637b5fcc2c0cef8848f
Davi Lima <tininhakawaii@gmail.com>
2024-12-11 21:02:21 -0300

    Tirei o exemplo

--- src/api/app.ts
+++ src/api/app.ts
@@ -18,21 +18,6 @@ app.use("/conta", ContaRouter);
 import { TarefaRouter } from "./Tarefa/TarefaRouter";
 app.use("/tarefa", TarefaRouter);
 
-////////////////////////////////////////////////////////////////
-
-app.get("/victor", (req, res) => {
-    superConsole(req); // <-------- Ignore essa função
-    const { email, senha } = req.body;
-
-    if (email === "victor@email.com" && senha === "123") {
-        res.status(200).send({ mensagem:"Login realizado com sucesso" });
-    } else {
-        res.status(401).send({ mensagem:"Credenciais inválidas" });
-    }
-});
-
-////////////////////////////////////////////////////////////////
-
 AppDataSource.initialize().then(() => {
     console.log("Conectado ao banco de dados")
 }).catch((error) => {
@@ -40,14 +25,3 @@ AppDataSource.initialize().then(() => {
 });
 
 export default app;
-
-function superConsole(req: any) {
-    console.log("Requisição recebida");
-    console.log("Método: ", req.method);
-    console.log("URL: ", req.url);
-    console.log("Corpo: ", req.body);
-    console.log("Query: ", req.query);
-    console.log("Parâmetros: ", req.params);
-    console.log("Cookies: ", req.cookies);
-    console.log("Cabeçalhos: ", req.headers);
-}
